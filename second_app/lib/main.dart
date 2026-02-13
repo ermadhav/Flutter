f3df020bff220f27f200076feee63781920500f0
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-13 13:53:50 +0530

    Updated

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -1,36 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(yApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
-  Widget build(BuilContext context){
-    return MaterialApp(home: LoginPage());
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
   }
 }
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
+
   @override
-  Widget build(BuilContext context){
+  Widget build(BuildContext context) {
     final userController = TextEditingController();
     final passController = TextEditingController();
+
     return Scaffold(
-      appBar: AppBar(title: text("Login page")),
+      appBar: AppBar(title: Text("Login page")),
       body: Padding(
         padding: EdgeInsets.all(15),
         child: Column(
-          children:[
+          children: [
             TextField(
               controller: userController,
-              decoration: InputDecoration(labelText: "User Page")
-            )
-          ]
-        )
-      )
-    )
+              decoration: InputDecoration(labelText: "Username"),
+            ),
+            TextField(
+              controller: passController,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
